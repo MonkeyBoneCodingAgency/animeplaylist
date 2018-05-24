3fb53c1d460e13fc0706c121ec2c56dd9b459d7c
godfb <brandonmgodfrey@gmail.com>
2018-05-24 14:09:52 -0700

    server now hosting html

--- index.js
+++ index.js
@@ -4,21 +4,36 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-const http = require('http')
-const port = 3000
+// const http = require('http')
+// const port = 3000
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(__dirname + ''));
+app.set('port', process.env.PORT || 3000);
 
-const server = http.createServer(requestHandler)
+var server = app.listen(app.get('port'), function() {
+    console.log('Server listening on port ' + server.address().port);
+});
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+// const requestHandler = (request, response) => {
+//   console.log(request.url)
+//   response.end('Hello Node.js Server!')
+// }
 
-  console.log(`server is listening on ${port}`)
-})
+// const server = http.createServer(requestHandler)
 
+// server.listen(port, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+
+//   console.log(`server is listening on ${port}`)
+// })
+
+// app.get('/', function(req, res) {
+//     console.log("inhere");
+//     res.send('hello world');
+//     res.end();
+// });
