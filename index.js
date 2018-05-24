a35bbe1597d74455fdbb007f504573f85217c40e
godfb <brandonmgodfrey@gmail.com>
2018-05-24 14:32:04 -0700

    added npm start command

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-// const http = require('http')
-// const port = 3000
-
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,23 +14,3 @@ var server = app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + server.address().port);
 });
 
-// const requestHandler = (request, response) => {
-//   console.log(request.url)
-//   response.end('Hello Node.js Server!')
-// }
-
-// const server = http.createServer(requestHandler)
-
-// server.listen(port, (err) => {
-//   if (err) {
-//     return console.log('something bad happened', err)
-//   }
-
-//   console.log(`server is listening on ${port}`)
-// })
-
-// app.get('/', function(req, res) {
-//     console.log("inhere");
-//     res.send('hello world');
-//     res.end();
-// });
