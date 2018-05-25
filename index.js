0dcdc622fe579b6f97dffb3b01ca90de3a27d650
godfreb <brandon.godfrey@autodesk.com>
2018-05-25 16:29:52 -0700

    fixed server hosting incorrect path

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', express.static(__dirname + ''));
+app.use('/', express.static(__dirname + '/client'));
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + server.address().port);
 });
 
+app.post('/submitUsername', function(req, res) {
+    console.log(req.body);
+    res.end();
+});
